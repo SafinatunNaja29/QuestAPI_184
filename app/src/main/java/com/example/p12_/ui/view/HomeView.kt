33194b2320e3cf175871ce4ea911f1ad2b36215c
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-30 05:48:46 +0700

    menambahkan Object DestinasiHome yang mengimplementasikan DestinasiNavigasi

---
+++ app/src/main/java/com/example/p12_/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.p12_.ui.view
+
+import com.example.p12_.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mahasiswa"
+}
