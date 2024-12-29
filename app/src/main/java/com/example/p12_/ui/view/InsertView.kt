15d7fb7093d48e39e4d43a1f27e32a93704b4fd0
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-30 05:38:22 +0700

    menambahkan Object DestinasiEntry yang mengimplementasikan DestinasiNavigasi

---
+++ app/src/main/java/com/example/p12_/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.p12_.ui.view
+
+import com.example.p12_.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Insert Mahasiswa"
+}
