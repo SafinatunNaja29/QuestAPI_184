f8813336909bfd20a45a229147ca82cbf70ff6aa
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-30 08:58:06 +0700

    menambahkan DetailView  untuk menampilkan detail mahasiswa dengan fitur

--- app/src/main/java/com/example/p12_/ui/view/DetailView.kt
+++ app/src/main/java/com/example/p12_/ui/view/DetailView.kt
@@ -1,5 +1,19 @@
 package com.example.p12_.ui.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import com.example.p12_.ui.customwidget.CostumeTopAppBar
 import com.example.p12_.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi {
@@ -7,4 +21,43 @@ object DestinasiDetail: DestinasiNavigasi {
     const val NIM = "nim"
     override val titleRes = "Detail Mahasiswa"
     val routeWithArg = "$route/{$NIM}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailView(
+    navigateBack: () -> Unit,
+    navigateToEdit: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToEdit,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Delete Mahasiswa"
+                )
+            }
+        }
+    ) { innerPadding ->
+        BodyDetailMhs(
+            detailUiState = viewModel.detailUiState,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
