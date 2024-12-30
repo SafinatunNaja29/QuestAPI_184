ef8b2e454f922c0d1195cf7b944c412d137cea40
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-30 09:07:12 +0700

    Menambahkan DetailUiState data class untuk mengelola status tampilan detail mahasiswa

---
+++ app/src/main/java/com/example/p12_/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.p12_.ui.viewmodel
+
+data class DetailUiState(
+    val detailUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == InsertUiEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != InsertUiEvent()
+}
